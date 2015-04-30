368234a2b69ec342ec4ce77cac4e5592b81a1845
JorgeCampo <jorge7campo@hotmail.com>
2015-04-30 12:22:31 +0200

    despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+//var alumnos = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,4 +10,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', quizController.author);
+
 module.exports = router;
