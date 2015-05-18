dc23bc09a7997b42030f4a907401ed23ffcfc4e7
JorgeCampo <jorge7campo@hotmail.com>
2015-05-18 16:10:01 +0200

    Autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-
+var sessionController = require('../controllers/session_controller');
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
@@ -10,6 +10,11 @@ router.param('quizId', quizController.load);
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
+// Definicion de rutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
