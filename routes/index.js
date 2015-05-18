fd5c20f82d21c9fab0928be5af2825d8a5e8fa3e
JorgeCampo <jorge7campo@hotmail.com>
2015-05-18 16:23:31 +0200

    Autorizacion

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,20 @@ router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
 
 
-router.get('/quizes', quizController.index);
+//Definicion de rutas de /quizes
+router.get('/quizes',  sessionController.loginRequired, quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/author', quizController.author);	//cambio realizado en la rama creditos
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', 		   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+
+router.get('/author',                      sessionController.loginRequired, quizController.author);//cambio realizado en la rama creditos
 
 
+//Definicion de rutas de comentarios
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
