8670e9fe8be1f58d84c4ffb3c6ac1e7d67541a27
JorgeCampo <jorge7campo@hotmail.com>
2015-05-21 19:09:10 +0200

    Registro y gestion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var userController = require('../controllers/user_controller');
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 router.param('commentId', commentController.load);
+router.param('userId', userController.load);  // autoload :userId
 
 /* GET home page. */
 router.get('/', sessionController.logout, function(req, res) {
@@ -15,6 +17,13 @@ router.get('/', sessionController.logout, function(req, res) {
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
+
+// Definición de rutas de cuenta
+router.get('/user',  userController.new);     // formulario sign un
+router.post('/user',  userController.create);     // registrar usuario
+router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.edit);     // editar información de cuenta
+router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.update);     // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userController.destroy);     // borrar cuenta
 
 
 
